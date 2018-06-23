4e14fb70309e049da040a41ff6bdd921b53019b5
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-06-23 23:44:26 +0300

    Updated demo project

--- Source/Fluency/fluencydemo/src/main/java/com/visuality/fluencydemo/MainActivity.java
+++ Source/Fluency/fluencydemo/src/main/java/com/visuality/fluencydemo/MainActivity.java
@@ -3,12 +3,6 @@ package com.visuality.fluencydemo;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.visuality.fluency.Letter;
-import com.visuality.fluency.Phrase;
-import com.visuality.fluency.Sentence;
-import com.visuality.fluency.SoundType;
-import com.visuality.fluency.Word;
-
 public class MainActivity extends Activity {
 
     @Override
